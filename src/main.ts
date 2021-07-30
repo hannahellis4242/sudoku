2f7e2304fc234eb73fb417e3c17e144cd14a6d1d
hannahellis4242 <hannahellis4242@gmail.com>
2021-07-30 01:25:47 +0100

    making functions actually work in async manner

--- src/main.ts
+++ src/main.ts
@@ -9,29 +9,71 @@ import SudokuProblem, { Entry } from "./Sudoku/Problem";
 import { reject } from "./Sudoku/reject";
 import { root } from "./Sudoku/root";
 
+const createProblemID = (p: SudokuProblem) => {
+  if (p.entries.length !== 0) {
+    return p.entries
+      .map((x: Entry) => x.index.toString() + "," + x.value.toString())
+      .reduce((acc: string, x: string) => acc + ":" + x);
+  } else {
+    return "";
+  }
+};
+
 type IndexValuePair = [number, number];
 type InputData = IndexValuePair[];
 
-fs.readFile("problem.json", (err: Error | null, data: Buffer) => {
-  if (err) {
-    console.error(err.message);
-  }
-  const input: InputData = JSON.parse(data.toString());
-  const entries: Entry[] = [];
-  for (const indexValuePair of input) {
-    entries.push(new Entry(indexValuePair[0], indexValuePair[1]));
-  }
-  const p = new SudokuProblem(entries);
-  const output = (p: SudokuProblem, c: SudokuCandidate) => {
-    console.log(new Grid(p, c).show());
-  };
-  solve<SudokuProblem, SudokuCandidate>(
-    p,
-    root,
-    first,
-    next,
-    reject,
-    accept,
-    output
-  );
+type Result = number[];
+type Results = Result[];
+class Solution {
+  constructor(public id: string, public results: Results) {}
+}
+
+const run = async (file: string) => {
+  return new Promise<Solution>((res, rej) => {
+    fs.readFile(file, (err: Error | null, data: Buffer) => {
+      if (err) {
+        rej(err);
+        return;
+      }
+      const input: InputData = JSON.parse(data.toString());
+      const entries: Entry[] = input.map(
+        (x: IndexValuePair) => new Entry(x[0], x[1])
+      );
+      const p = new SudokuProblem(entries);
+      const id = createProblemID(p);
+      const results: Results = [];
+      const output = (p: SudokuProblem, c: SudokuCandidate) => {
+        results.push(
+          new Grid(p, c).values.map((x: number | null) => {
+            if (x) {
+              return x;
+            } else {
+              return -1;
+            }
+          })
+        );
+      };
+      solve<SudokuProblem, SudokuCandidate>(
+        p,
+        root,
+        first,
+        next,
+        reject,
+        accept,
+        output
+      ).then(() => {
+        res(new Solution(id, results));
+      });
+    });
+  });
+};
+
+const list = ["problem.json", "problem2.json", "p3.json"];
+list.forEach((file: string) => {
+  run(file)
+    .then((s: Solution) => console.log(s))
+    .catch((err: Error) => {
+      console.error(err.message);
+    });
 });
+console.log("main done");
