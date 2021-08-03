1cb7d0581e737313f9316bbaf79246e724e8a9eb
Hannah Ellis <hannahellis4242@gmail.com>
2021-08-03 23:37:13 +0100

    tcp server works ok now. Lots to clean down

---
+++ src/server.ts
@@ -0,0 +1,87 @@
+import { Server, Socket } from "net";
+import { createProblem, run, Solution, createID, InputData } from "./utils/run";
+import { Validator } from "jsonschema";
+
+const schema = {
+  $schema: "https://json-schema.org/draft/2019-09/schema",
+  type: "array",
+  items: {
+    type: "array",
+    items: [
+      {
+        type: "number",
+      },
+      {
+        type: "number",
+      },
+    ],
+  },
+};
+
+const isValidJSON = (raw: object) => {
+  return new Validator().validate(raw, schema);
+};
+
+const parseJSON = (str: string): object | Error => {
+  try {
+    return JSON.parse(str);
+  } catch (err) {
+    return new Error(err.mesage);
+  }
+};
+
+const handleConnection = (sock: Socket) => {
+  const date = new Date();
+  const remoteAddress = sock.remoteAddress + ":" + sock.remotePort;
+  console.log(date, " > new client connection from ", remoteAddress);
+  sock.on("data", (data: Buffer) => {
+    console.log(date, " > data :", data.toString());
+    const raw = parseJSON(data.toString());
+    if (raw instanceof Error) {
+      sock.write(
+        JSON.stringify({
+          error: "parse",
+          input: data.toString(),
+          mesage: "could not parse json",
+          result: raw.message.toString(),
+        })
+      );
+      sock.destroy();
+    } else {
+      console.log(date, " > parsed : ", JSON.stringify(raw));
+      const result = isValidJSON(raw);
+      if (result.valid) {
+        const p = createProblem(raw as InputData);
+        console.log(date, " > ", createID(p));
+        run(p).then((s: Solution) => {
+          sock.write(JSON.stringify(s));
+          sock.destroy();
+        });
+      } else {
+        sock.write(
+          JSON.stringify({
+            error: "json",
+            input: data.toString(),
+            mesage: "could not convert input to json",
+            result: result.toString(),
+          })
+        );
+        sock.destroy();
+      }
+    }
+
+    sock.once("close", () => {
+      console.log("connection from %s closed", remoteAddress);
+    });
+    sock.on("error", (err: Error) => {
+      console.log("Connection %s error: %s", remoteAddress, err.message);
+      sock.destroy();
+    });
+  });
+};
+
+const server = new Server();
+server.on("connection", handleConnection);
+server.listen(3000, function () {
+  console.log("server listening to %j", server.address());
+});
--- src/test.ts
+++ src/test.ts
@@ -1,18 +1,23 @@
+const foo = (x: number, acc: number, n: number): number => {
+  if (n < 0) {
+    return acc;
+  } else {
+    return foo(x + 1, acc + x * x, n - 1);
+  }
+};
+
 const f = async (n: number) => {
   const start = new Date().getTime();
-  let x = 0;
-  for (let i = 0; i < n; ++i) {
-    x += i * i;
-  }
+  const result = foo(0, 0, n);
   const end = new Date().getTime();
   console.log(n, " started ", start, " ended ", end, " elased ", end - start);
-  return x;
+  return result;
 };
 
 const g = async () => {
   console.log("g start");
   for (let i = 0; i < 10; ++i) {
-    f(10000000000 * i).then((x: number) => console.log(i, " -> ", x));
+    f(1000 * i).then((x: number) => console.log(i, " -> ", x));
     console.log("started");
   }
   console.log("g done");
@@ -21,7 +26,7 @@ const g = async () => {
 g();
 console.log("main start");
 for (let i = 0; i < 4; ++i) {
-  f(10000000000 * i).then((x: number) => console.log(i, " -> ", x));
+  f(100 * i).then((x: number) => console.log(i, " -> ", x));
   console.log("started");
 }
 console.log("main done");
