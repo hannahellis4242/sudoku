f73f9fe2760534baca9a8be396fc21f780da8cf0
Hannah Ellis <hannahellis4242@gmail.com>
2021-08-10 00:04:23 +0100

    simplifying the webserver code to just serve up the public directory as a static website

--- webserver/src/main.ts
+++ webserver/src/main.ts
@@ -6,25 +6,7 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 app.use("/solver", router);
-app.use("/img", express.static(path.join(__dirname, "../public/img")));
-app.get("/", (_: Request, res: Response) =>
-  res.sendFile(path.join(__dirname, "../public/index.html"))
-);
-app.get("/index.html", (_: Request, res: Response) =>
-  res.sendFile(path.join(__dirname, "../public/index.html"))
-);
-app.get("/help.html", (_: Request, res: Response) =>
-  res.sendFile(path.join(__dirname, "../public/help.html"))
-);
-app.get("/about.html", (_: Request, res: Response) =>
-  res.sendFile(path.join(__dirname, "../public/about.html"))
-);
-app.get("/style.css", (_: Request, res: Response) =>
-  res.sendFile(path.join(__dirname, "../public/style/style.css"))
-);
-app.get("/scripts/app.js", (_: Request, res: Response) =>
-  res.sendFile(path.join(__dirname, "../public/scripts/app.js"))
-);
+app.use("/", express.static(path.join(__dirname, "../public")));
 app.get("*", (req: Request, res: Response) => {
   console.log(req.url);
   res.status(404).send();
